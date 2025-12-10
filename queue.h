d0f73a8d388e7f452e1fa8634a16494d34f9f7bb
lethelee99 <lelethelee@gmail.com>
2025-12-10 10:54:32 +0800

    Refactor queue.h to include age and reason in enqueue

--- queue.h
+++ queue.h
@@ -2,14 +2,21 @@
 #define QUEUE_H
 
 #include <string>
+using namespace std;
 
 const int MAX = 100;
 
-void enqueue(const std::string &name);
-void dequeue();
-void display();
+extern string nameArr[MAX];
+extern int ageArr[MAX];
+extern string reasonArr[MAX];
+
+extern int frontIndex;
+extern int rearIndex;
 
 bool isFull();
 bool isEmpty();
+void enqueue(string name, int age, string reason);
+void dequeue();
+void displayQueue();
 
 #endif
