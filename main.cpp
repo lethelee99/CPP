38bfcfc97e2ca0af8435f122407a6e2206d31baf
lethelee99 <lelethelee@gmail.com>
2025-12-10 10:53:36 +0800

    Enhance patient addition with age and reason
    Updated the hospital queue system to include age and reason for visit when adding a patient. Modified menu options and corresponding actions.

--- main.cpp
+++ main.cpp
@@ -1,47 +1,56 @@
 #include <iostream>
-#include <string>
 #include "queue.h"
 
 using namespace std;
 
 int main() {
     int choice;
-    string name;
+    string name, reason;
+    int age;
 
     do {
-        cout << "\n===== HOSPITAL QUEUE SYSTEM =====\n";
-        cout << "[1] Add Patient\n";
-        cout << "[2] Serve Next Patient\n";
-        cout << "[3] View Queue\n";
-        cout << "[0] Exit\n";
-        cout << "Choose option: ";
+        cout << "\n--- HOSPITAL QUEUE SYSTEM ---\n\n";
+        cout << "1. Add Patient\n";
+        cout << "2. Serve Patient\n";
+        cout << "3. Display Queue\n";
+        cout << "4. Exit\n";
+        cout << "\nChoose option: ";
         cin >> choice;
-        cin.ignore();  
 
         switch (choice) {
             case 1:
-                cout << "Enter patient name: ";
+                cout <<"\n===New Patient Forms===\n";
+                cout << "\nEnter patient name: ";
+                cin.ignore();
                 getline(cin, name);
-                enqueue(name);
-                break;              
+
+                cout << "Enter age: ";
+                cin >> age;
+
+                cout << "Enter Reason for Visit: ";
+                cin.ignore();
+                getline(cin, reason);
+
+                enqueue(name, age, reason);
+                break;
 
             case 2:
                 dequeue();
                 break;
 
             case 3:
-                display();
+                displayQueue();
                 break;
 
-            case 0:
-                cout << "Exiting...\n";
+            case 4:
+                cout << "Exiting system...\n";
                 break;
 
             default:
-                cout << "Invalid choice!\n";
+                cout << "Invalid option!\n";
         }
 
-    } while (choice != 0);
+    } while (choice != 4);
 
     return 0;
 }
